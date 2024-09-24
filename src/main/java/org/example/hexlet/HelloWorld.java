5b2c3fe9e7fe79120ab480a061b7d0eec2efcd55
Turich79 <turich79@mail.ru>
2024-09-24 10:16:04 +0700

    2409

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,10 +4,18 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        var app=Javalin.create(config->{
+        var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        app.get("/",ctx->ctx.result("Hello World"));
+        //less 1
+        app.get("/", ctx -> ctx.result("Hello World"));
+        //less 2
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        //less 3
+        app.get("/hello", ctx -> {
+            ctx.result("Hello new World");
+        });
         app.start(7070);
     }
 }
