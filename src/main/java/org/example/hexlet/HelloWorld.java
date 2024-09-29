4a04ffb5e27cada0430e5348c72bcc8cf6a7dcc5
Turich79 <turich79@mail.ru>
2024-09-29 10:44:56 +0700

    2909

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,18 +4,38 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        less5();
+//        less4();
+//        less3();
+    }
+
+    public static void less5() {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        //less 1
-        app.get("/", ctx -> ctx.result("Hello World"));
-        //less 2
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World!");
+//            var name=ctx.queryParam("name")
+            var congratulation = "Hello, " + name;
+            ctx.result(congratulation);
+        });
+        app.start(7070);
+    }
+
+    public static void less4() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
         app.get("/users", ctx -> ctx.result("GET /users"));
         app.post("/users", ctx -> ctx.result("POST /users"));
-        //less 3
-        app.get("/hello", ctx -> {
-            ctx.result("Hello new World");
+        app.start(7070);
+    }
+
+    public static void less3() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
         });
+        app.get("/", ctx -> ctx.result("Hello World"));
         app.start(7070);
     }
 }
